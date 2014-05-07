173aaac7ea6f123f5e69412f8f707a25e091e0fa
Matth_eu <matthew@matth.eu>
2014-05-07 21:08:08 +0100

    Update Gruntfile - tabs not spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,35 @@
 module.exports = function ( grunt ) {
-    grunt.initConfig( {
-        pkg : grunt.file.readJSON( 'package.json' ),
-        uglify : {
-            js : {
+	grunt.initConfig( {
+		pkg : grunt.file.readJSON( 'package.json' ),
+		uglify : {
+			js : {
 				options: {
 					sourceMap: true
 				},
-                files : {
-                    'build/js/wp-api.min.js' : [
-	                    'js/app.js',
-	                    'js/utils.js',
-	                    'js/models.js',
-	                    'js/views.js',
-	                    'js/collections.js'
-                	]
-                }
-            }
-        },
-        qunit: {
-            all: [ 'tests/*.html' ]
-        },
-        watch : {
+				files : {
+					'build/js/wp-api.min.js' : [
+						'js/app.js',
+						'js/utils.js',
+						'js/models.js',
+						'js/views.js',
+						'js/collections.js'
+					]
+				}
+			}
+		},
+		qunit: {
+			all: [ 'tests/*.html' ]
+		},
+		watch : {
 			files : [
-                'js/*.js'
-            ],
+				'js/*.js'
+			],
 			tasks : [ 'concat', 'uglify' ]
 		}
-    });
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
-    grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-    grunt.registerTask( 'default', [ 'uglify:js' ] );
-    grunt.registerTask( 'qunit', [ 'qunit:all' ] );
+	});
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+	grunt.registerTask( 'default', [ 'uglify:js' ] );
+	grunt.registerTask( 'qunit', [ 'qunit:all' ] );
 };
