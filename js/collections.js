b25b88d6d5e7de2cb2e44a0439dbfe72a7c4c45d
Taylor Lovett <admin@taylorlovett.com>
2014-05-07 23:59:37 -0400

    Add more fields to default user model; create users collection

--- js/collections.js
+++ js/collections.js
@@ -5,10 +5,20 @@
     /**
      * wp.api.collections.Posts
      */
-    wp.api.collections.Posts = Backbone.Collection.extend({
+    wp.api.collections.Posts = Backbone.Collection.extend( {
         url: WP_API_Settings.root + "/posts",
 
         model: wp.api.models.Post
+    });
+
+    /**
+     * Backbone users collection
+     * @type {*}
+     */
+    wp.api.collections.Users = Backbone.Collection.extend( {
+        url: WP_API_Settings.root + "/users",
+
+        model: wp.api.models.User
     });
 
 } )( wp, WP_API_Settings, Backbone, window, undefined );
