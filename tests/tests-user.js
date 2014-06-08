850c04f2931930af1346a05e1ad3f96a1c96e412
Taylor Lovett <admin@taylorlovett.com>
2014-06-08 18:50:30 -0400

    More user unit tests

--- tests/tests-user.js
+++ tests/tests-user.js
@@ -34,3 +34,43 @@ test( 'User model can be instantiated with correct default values', function() {
 	equal( user.get('URL'), '', 'Default URL should be empty' );
 	equal( user.get('avatar'), '', 'Default avatar should be empty' );
 });
+
+test( 'User model data can be set', function() {
+
+	expect ( 22 );
+
+	// Instantiate 2 new User models.
+	// 1 - setting data after creating the model.
+	// 2 - passing testUserData when creating.
+	var user1 = new wp.api.models.User();
+	var user2 = new wp.api.models.User( testUserData );
+
+	for ( var key in testUserData ) {
+
+		user1.set( key, testUserData[key] );
+
+		deepEqual( user1.get( key ), testUserData[key], 'User1 ' + key + ' should be set correctly' );
+		deepEqual( user2.get( key ), testUserData[key], 'User2 ' + key + ' should be set correctly' );
+
+	}
+
+});
+
+test( 'User model toJSON', function() {
+
+	expect( 7 );
+
+	var post = new wp.api.models.Post( testData );
+	var postJSON = post.toJSON();
+
+	// Check that dates are correctly converted to a string.
+	equal( postJSON.date, post.get( 'date' ).toISOString() );
+	equal( postJSON.date, post.get( 'modified' ).toISOString() );
+
+	// Check that user is setup correctly
+	equal( postJSON.author.get( 'ID' ), 1 );
+	equal( postJSON.author.get( 'username' ), 'wordpress' );
+	equal( postJSON.author.get( 'first_name' ), 'Word' );
+	equal( postJSON.author.get( 'last_name' ), 'Press' );
+	equal( postJSON.author.get( 'email' ), 'generic@wordpress.org' );
+});
