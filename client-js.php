4f700745b763691ce6437c7b1e488d1ed8b0bf0e
Taylor Lovett <admin@taylorlovett.com>
2014-06-12 19:01:32 -0400

    Check if WP API function exists before enqueuing JS stuff

--- client-js.php
+++ client-js.php
@@ -4,6 +4,13 @@
  */
 
 function json_api_client_js() {
+	/**
+	 * Check if WP API functionality exists. Not using is_plugin_active in prepartion for
+	 */
+	if ( ! function_exists( 'json_get_url_prefix' ) ) {
+		return;
+	}
+
 	wp_enqueue_script( 'wp-api-js', plugins_url( 'build/js/wp-api.min.js', __FILE__ ), array( 'jquery', 'underscore', 'backbone' ), '1.0', true );
 
 	$settings = array( 'root' => home_url( json_get_url_prefix() ), 'nonce' => wp_create_nonce( 'wp_json' ) );
