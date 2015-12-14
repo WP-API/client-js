6d6564f87597d368b4f2ef4f39aa04c62aee4259
adamsilverstein <adam@10up.com>
2015-12-14 08:42:36 -0700

    Correct version string default

--- js/app.js
+++ js/app.js
@@ -10,6 +10,6 @@
 
 	window.wp            = window.wp || {};
 	wp.api               = wp.api || new WP_API();
-	wp.api.versionString = wp.api.versionString || 'wp/v2';
+	wp.api.versionString = wp.api.versionString || 'wp/v2/';
 
 })( window );
