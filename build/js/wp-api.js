53784e8e16e35e8cef4f5babcabeb6eb499be63e
adamsilverstein <adam@10up.com>
2016-02-22 22:37:29 -0700

    Grunt

--- build/js/wp-api.js
+++ build/js/wp-api.js
@@ -454,11 +454,14 @@
 				 * Uses the embedded data if available, otherwises fetches the
 				 * data from the server.
 				 *
-				 * @return {Deferred.promise} promise Resolves to a wp.api.collections.PostCategories
-				 * collection containing the post categories.
+				 * @return {Deferred.promise} promise Resolves to an array of wp.api.models.Category
+				 *                                    objects.
 				 */
 				getCategories: function() {
-					return buildCollectionGetter( this, 'PostCategories', 'https://api.w.org/term', 0 );
+					var categoryIds = this.get( 'categories' ),
+						categories  = new wp.api.collections.Categories();
+
+					return categories.fetch( { data: { include: categoryIds } } );
 				},
 
 				/**
@@ -474,17 +477,22 @@
 						self = this,
 						newCategories = [];
 
+					if ( _.isString( categories ) ) {
+						return false;
+					}
+
 					// If this is an array of slugs, build a collection.
 					if ( _.isArray( categories ) ) {
 
 						// Get all the categories.
 						allCategories = new wp.api.collections.Categories();
 						allCategories.fetch( {
+							data:    { per_page: 100 },
 							success: function( allcats ) {
 
 								// Find the passed categories and set them up.
 								_.each( categories, function( category ) {
-									newCategory = new wp.api.models.PostCategories( allcats.findWhere( { slug: category } ) );
+									newCategory = new wp.api.models.Category( allcats.findWhere( { slug: category } ) );
 
 									// Tie the new category to the post.
 									newCategory.set( 'parent_post', self.get( 'id' ) );
@@ -492,7 +500,7 @@
 									// Add the new category to the collection.
 									newCategories.push( newCategory );
 								} );
-								categories = new wp.api.collections.PostCategories( newCategories );
+								categories = new wp.api.collections.Categories( newCategories );
 								self.setCategoriesWithCollection( categories );
 							}
 						} );
@@ -512,31 +520,10 @@
 				 *
 				 */
 				setCategoriesWithCollection: function( categories ) {
-					var removedCategories, addedCategories, categoriesIds, existingCategoriesIds;
 
-					// Get the existing categories.
-					this.getCategories().done( function( existingCategories ) {
-
-						// Pluck out the category ids.
-						categoriesIds         = categories.pluck( 'id' );
-						existingCategoriesIds = existingCategories.pluck( 'id' );
-
-						// Calculate which categories have been removed or added (leave the rest).
-						addedCategories   = _.difference( categoriesIds, existingCategoriesIds );
-						removedCategories = _.difference( existingCategoriesIds, categoriesIds );
-
-						// Add the added categories.
-						_.each( addedCategories, function( addedCategory ) {
-
-							// Save the new categories on the post with a 'POST' method, not Backbone's default 'PUT'.
-							existingCategories.create( categories.get( addedCategory ), { type: 'POST' } );
-						} );
-
-						// Remove the removed categories.
-						_.each( removedCategories, function( removedCategory ) {
-							existingCategories.get( removedCategory ).destroy();
-						} );
-					} );
+					// Pluck out the category ids.
+					this.set( 'categories', categories.pluck( 'id' ) );
+					this.save();
 				}
 			},
 
@@ -559,13 +546,13 @@
 			};
 
 		// Exit if we don't have valid model defaults.
-		if ( _.isUndefined( model.defaults ) ) {
+		if ( _.isUndefined( model.prototype.args ) ) {
 			return model;
 		}
 
 		// Go thru the parsable date fields, if our model contains any of them it gets the TimeStampedMixin.
 		_.each( parseableDates, function( theDateKey ) {
-			if ( ! _.isUndefined( model.defaults[ theDateKey ] ) ) {
+			if ( ! _.isUndefined( model.prototype.args[ theDateKey ] ) ) {
 				hasDate = true;
 			}
 		} );
@@ -576,17 +563,17 @@
 		}
 
 		// Add the AuthorMixin for models that contain an author.
-		if ( ! _.isUndefined( model.defaults.author ) ) {
+		if ( ! _.isUndefined( model.prototype.args.author ) ) {
 			model = model.extend( AuthorMixin );
 		}
 
 		// Add the FeaturedImageMixin for models that contain a featured_image.
-		if ( ! _.isUndefined( model.defaults.featured_image ) ) {
+		if ( ! _.isUndefined( model.prototype.args.featured_image ) ) {
 			model = model.extend( FeaturedImageMixin );
 		}
 
 		// Add the CategoriesMixin for models that support categories collections.
-		if ( ! _.isUndefined( loadingObjects.collections[ modelClassName + 'Categories' ] ) ) {
+		if ( ! _.isUndefined( model.prototype.args.categories ) ) {
 			model = model.extend( CategoriesMixin );
 		}
 
@@ -596,7 +583,7 @@
 		}
 
 		// Add the TagsMixin for models that support tags collections.
-		if ( ! _.isUndefined( loadingObjects.collections[ modelClassName + 'Tags' ] ) ) {
+		if ( ! _.isUndefined( loadingObjects.collections.tags ) ) {
 			model = model.extend( TagsMixin );
 		}
 
@@ -1001,7 +988,7 @@
 				) {
 
 					// Single items end with a regex (or the special case 'me').
-					if ( index.endsWith( '+)' ) || index.endsWith( 'me' ) ) {
+					if ( /.*[+)|me]$/.test( index ) ) {
 						modelRoutes.push( { index: index, route: route } );
 					} else {
 
@@ -1235,6 +1222,6 @@
 	 */
 
 	// The wp.api.init function returns a promise that will resolve with the endpoint once it is ready.
-	wp.api.init();
+	wp.api.loadPromise = wp.api.init();
 
 } )();
