a1874deb5d4d2427bf9cc6ac2033465fd1993885
adamsilverstein <adam@10up.com>
2016-03-02 13:22:35 -0700

    Only enqueue oauth files if oauth available in schema

--- client-js.php
+++ client-js.php
@@ -5,19 +5,11 @@
 
 function json_api_client_js() {
 
-	$scripts = wp_scripts();
-	$src = plugins_url( 'build/js/wp-api.js', __FILE__ );
-
-	if ( isset( $scripts->registered['wp-api'] ) ) {
-		$scripts->registered['wp-api']->src = $src;
-	} else {
-		wp_register_script( 'wp-api', $src, array( 'jquery', 'underscore', 'backbone' ), '1.0', true );
-	}
-
 	/**
 	 * @var \WP_REST_Server $wp_rest_server
 	 */
 	global $wp_rest_server;
+
 	if ( empty( $wp_rest_server ) ) {
 		/** This filter is documented in wp-includes/rest-api.php */
 		$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
@@ -27,23 +19,43 @@ function json_api_client_js() {
 		do_action( 'rest_api_init', $wp_rest_server );
 	}
 
-	$schema_request = new WP_REST_Request( 'GET', '/wp/v2' );
-	$schema_response = $wp_rest_server->dispatch( $schema_request );
-	$schema = null;
-	if ( ! $schema_response->is_error() ) {
-		$schema = $schema_response->get_data();
-	}
-
 	// Search for the OAuth1 authentication, and add any info to localized data.
 	$oauth_request = new WP_REST_Request( 'GET', '/' );
 	$oauth_response = $wp_rest_server->dispatch( $oauth_request );
 	$oauth1 = null;
+
 	if ( ! $oauth_response->is_error() ) {
 		$oauth_data = $oauth_response->get_data();
 
 		if ( isset(  $oauth_data['authentication']['oauth1'] ) ) {
 			$oauth1 =  $oauth_data['authentication']['oauth1'];
 		}
+	}
+
+	// Set up OAuth if available.
+	if ( $oauth1 ) {
+		wp_enqueue_script( 'sha1', plugins_url( 'vendor/js/hmac-sha1.js', __FILE__ ), array()  );
+		wp_enqueue_script( 'sha256', plugins_url( 'vendor/js/hmac-sha256.js', __FILE__ ), array()  );
+		wp_enqueue_script( 'base64', plugins_url( 'vendor/js/enc-base64-min.js', __FILE__ ), array()  );
+		wp_enqueue_script( 'oauth1', plugins_url( 'vendor/js/oauth-1.0a.js', __FILE__ ), array( 'base64', 'sha1', 'sha256' )  );
+	}
+
+
+	$scripts = wp_scripts();
+	$src = plugins_url( 'build/js/wp-api.js', __FILE__ );
+	if ( isset( $scripts->registered['wp-api'] ) ) {
+		$scripts->registered['wp-api']->src = $src;
+	} else {
+		wp_register_script( 'wp-api', $src, array( 'jquery', 'underscore', 'backbone' ), '1.0', true );
+	}
+
+
+
+	$schema_request = new WP_REST_Request( 'GET', '/wp/v2' );
+	$schema_response = $wp_rest_server->dispatch( $schema_request );
+	$schema = null;
+	if ( ! $schema_response->is_error() ) {
+		$schema = $schema_response->get_data();
 	}
 
 	$settings = array(
@@ -55,7 +67,8 @@ function json_api_client_js() {
 		'oauth1Token'    => isset( $_GET['oauth_token'] ) ? sanitize_text_field( $_GET['oauth_token'] ) : null,
 		'oauth1Verifier' => isset( $_GET['oauth_verifier'] ) ? sanitize_text_field( $_GET['oauth_verifier'] ) : null,
 		'oauth1Public'    => '0XKFJPpIuBWR',
-		'oauth1Secret'    => 'SFh0EqddY1dwhiq2G7GvExEQdMY89TyT0C05qpQELJPFlS7R'
+		'oauth1Secret'    => 'SFh0EqddY1dwhiq2G7GvExEQdMY89TyT0C05qpQELJPFlS7R',
+		'loggedInCookie'  => LOGGED_IN_COOKIE
 	);
 	wp_localize_script( 'wp-api', 'wpApiSettings', $settings );
 
@@ -64,19 +77,4 @@ function json_api_client_js() {
 if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
 	add_action( 'wp_enqueue_scripts', 'json_api_client_js' );
 	add_action( 'admin_enqueue_scripts', 'json_api_client_js' );
-}
-
-function add_oauth_scripts() {
-	echo '
-<!-- sha1 -->
-<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/hmac-sha1.js"></script>
-<!-- sha256 -->
-<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/hmac-sha256.js"></script>
-
-<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/enc-base64-min.js"></script>
-<script src="' . esc_url( plugins_url( 'oauth-1.0a.js', __FILE__ ) ) . '"></script>';
-}
-// Is OAuth1 enabled?
-if ( true ) {
-	add_action( 'wp_head', 'add_oauth_scripts' );
 }
