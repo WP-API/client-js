0477848365726f990cc6979c42b40d47b7f64488
Adam Silverstein <adam@10up.com>
2016-06-25 11:47:12 +0200

    Indentation cleanup, add inline docs

--- client-js.php
+++ client-js.php
@@ -3,8 +3,12 @@
  * Plugin Name: WP-API Client JS.
  */
 
+/**
+ * Set up the REST API server and localize the schema.
+ */
 function json_api_client_js() {
 
+	// Ensure that the wp-api script is registered.
 	$scripts = wp_scripts();
 	$src = plugins_url( 'build/js/wp-api.js', __FILE__ );
 	if ( isset( $scripts->registered['wp-api'] ) ) {
@@ -17,34 +21,35 @@ function json_api_client_js() {
 	 * @var WP_REST_Server $wp_rest_server
 	 */
 	global $wp_rest_server;
+
+	// Ensure the rest server is intiialized.
 	if ( empty( $wp_rest_server ) ) {
 		/** This filter is documented in wp-includes/rest-api.php */
 		$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
-		$wp_rest_server = new $wp_rest_server_class();
+		$wp_rest_server       = new $wp_rest_server_class();
 
 		/** This filter is documented in wp-includes/rest-api.php */
 		do_action( 'rest_api_init', $wp_rest_server );
 	}
 
-	$schema_request = new WP_REST_Request( 'GET', '/wp/v2' );
+	// Load the schema.
+	$schema_request  = new WP_REST_Request( 'GET', '/wp/v2' );
 	$schema_response = $wp_rest_server->dispatch( $schema_request );
 	$schema = null;
 	if ( ! $schema_response->is_error() ) {
 		$schema = $schema_response->get_data();
 	}
 
+	// Localize the plugin serrings and schema.
 	$settings = array(
-		'root' => esc_url_raw( get_rest_url() ),
-		'nonce' => wp_create_nonce( 'wp_rest' ),
+		'root'          => esc_url_raw( get_rest_url() ),
+		'nonce'         => wp_create_nonce( 'wp_rest' ),
 		'versionString' => 'wp/v2/',
-		'schema' => $schema,
+		'schema'        => $schema,
 	);
 	wp_localize_script( 'wp-api', 'wpApiSettings', $settings );
 
 }
 
-if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
-	add_action( 'wp_enqueue_scripts', 'json_api_client_js' );
-	add_action( 'admin_enqueue_scripts', 'json_api_client_js' );
-}add_action( 'wp_enqueue_scripts', 'json_api_client_js' );
+add_action( 'wp_enqueue_scripts', 'json_api_client_js' );
 add_action( 'admin_enqueue_scripts', 'json_api_client_js' );
