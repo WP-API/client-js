7a3e212fc60d0a9aff30749367a4608687170282
Adam Silverstein <adam@10up.com>
2016-12-11 12:40:24 -0500

    Add tests for collections with attached helpers, are the helpers functions on the collection models?

--- tests/wp-api.js
+++ tests/wp-api.js
@@ -32,13 +32,67 @@ _.each( collectionClassNames, function( className ) {
 	QUnit.test( 'Testing ' + className + ' collection.', function( assert ) {
 		var done = assert.async();
 
-		assert.expect( 2 );
-
 		wp.api.loadPromise.done( function() {
 			var theCollection = new wp.api.collections[ className ]();
-			assert.ok( theCollection, 'We can instantiate wp.api.collections.' + className );
+			assert.ok(
+				theCollection,
+				'We can instantiate wp.api.collections.' + className
+			);
 			theCollection.fetch().done( function() {
-				assert.equal( 1, theCollection.state.currentPage, 'We should be on page 1 of the collection in ' + className  );
+				assert.equal(
+					1,
+					theCollection.state.currentPage,
+					'We should be on page 1 of the collection in ' + className
+				);
+
+					// Test that collection models get helper methods.
+					var typesWithHelpers = [
+						{
+							'collectionType':   'Posts',
+							'returnsModelType': 'post',
+							'supportsMethods':  {
+								'getDate': 'getDate',
+								/*'getMeta': 'getMeta', currently not supported */
+								'getRevisions': 'getRevisions',
+								'getTags': 'getTags',
+								'getCategories': 'getCategories',
+								'getAuthorUser': 'getAuthorUser',
+								'getFeaturedMedia': 'getFeaturedMedia'
+							}
+						},
+						{
+							'collectionType':   'Pages',
+							'returnsModelType': 'page',
+							'supportsMethods':  {
+								'getDate': 'getDate',
+								'getRevisions': 'getRevisions',
+								'getAuthorUser': 'getAuthorUser',
+								'getFeaturedMedia': 'getFeaturedMedia'
+							}
+						}
+
+					];
+					var matchingHelper = _.findWhere( typesWithHelpers, { 'collectionType': className } );
+					if ( ! _.isUndefined( matchingHelper ) ) {
+
+						// Test the returned model.
+						var firstModel = theCollection.at( 0 );
+						assert.equal(
+							matchingHelper.returnsModelType,
+							firstModel.get( 'type' ),
+							'The wp.api.collections.' + className + ' is of type ' + matchingHelper.returnsModelType
+						);
+
+						_.each( matchingHelper.supportsMethods, function( method ) {
+							assert.equal(
+								'function',
+								typeof firstModel[ method ],
+								className + '.' + method + ' is a function.'
+							)
+						} );
+					}
+
+
 				done();
 			} );
 
